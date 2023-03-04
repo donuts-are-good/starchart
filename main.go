3fc48151d12df5befe5bcbbdf15456acab90700b
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-03-03 22:46:27 -0600

    idk what this was

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 
@@ -11,11 +12,19 @@ import (
 
 func main() {
 
-	// the user
-	owner := "donuts-are-good"
+	// parse the command line arguments
+	var (
+		user = flag.String("user", "", "The GitHub user or organization that owns the repository")
+		repo = flag.String("repo", "", "The name of the GitHub repository")
+	)
+	flag.Parse()
 
-	// the repo
-	repo := "bearclaw"
+	// check both user and repo
+	if *user == "" || *repo == "" {
+		fmt.Fprintln(os.Stderr, "Error: Both the --user and --repo flags must be provided.")
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	// creating a background context as a requirement of oauth
 	ctx := context.Background()
@@ -40,7 +49,7 @@ func main() {
 	for {
 
 		// fetch them stargazers
-		stargazers, resp, err := client.Activity.ListStargazers(ctx, owner, repo, opt)
+		stargazers, resp, err := client.Activity.ListStargazers(ctx, *user, *repo, opt)
 		if err != nil {
 			fmt.Printf("Error fetching stargazers: %v\n", err)
 			os.Exit(1)
@@ -59,7 +68,7 @@ func main() {
 	}
 
 	// before we print the list, we tell the user how many results there were
-	fmt.Printf("Found %d stargazers for %s/%s\n", len(allStargazers), owner, repo)
+	fmt.Printf("Found %d stargazers for %p/%p\n", len(allStargazers), user, repo)
 
 	// format this into something that will make a grid in your readme
 	for _, user := range allStargazers {
